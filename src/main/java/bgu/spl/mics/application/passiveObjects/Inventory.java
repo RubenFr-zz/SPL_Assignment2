0833838b78bf585a11e695aa8e51bedbcce29b43
RubenFr <ruben.fratty@gmail.com>
2019-12-09 22:02:54 +0200

    did invetory class and tester

--- src/main/java/bgu/spl/mics/application/passiveObjects/Inventory.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Inventory.java
@@ -1,9 +1,14 @@
 package main.java.bgu.spl.mics.application.passiveObjects;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
- *  That's where Q holds his gadget (e.g. an explosive pen was used in GoldenEye, a geiger counter in Dr. No, etc).
+ * That's where Q holds his gadget (e.g. an explosive pen was used in GoldenEye, a geiger counter in Dr. No, etc).
  * <p>
  * This class must be implemented safely as a thread-safe singleton.
  * You must not alter any of the given public methods of this class.
@@ -11,45 +16,66 @@ import java.util.List;
  * You can add ONLY private fields and methods to this class as you see fit.
  */
 public class Inventory {
-	private List<String> gadgets;
-	/**
+    private List<String> gadgets;
+
+    private Inventory() {
+        this.gadgets = new LinkedList<>();
+    }
+
+    /**
+     * Static inner class (Bill Push singleton method)
+     * That way we are sure the class instance is only defined once !
+     */
+    private static class InventoryHolder {
+        private static Inventory instance = new Inventory();
+    }
+
+    /**
      * Retrieves the single instance of this class.
      */
-	public static Inventory getInstance() {
-		//TODO: Implement this
-		return null;
-	}
+    public static Inventory getInstance() {
+        return InventoryHolder.instance;
+    }
 
-	/**
+    /**
      * Initializes the inventory. This method adds all the items given to the gadget
      * inventory.
      * <p>
-     * @param inventory 	Data structure containing all data necessary for initialization
-     * 						of the inventory.
+     *
+     * @param inventory Data structure containing all data necessary for initialization
+     *                  of the inventory.
      */
-	public void load (String[] inventory) {
-		//TODO: Implement this
-	}
-	
-	/**
+    public void load(String[] inventory) {
+        gadgets.addAll(Arrays.asList(inventory));
+    }
+
+    public List<String> getGadgets() {
+        return gadgets;
+    }
+
+    /**
      * acquires a gadget and returns 'true' if it exists.
      * <p>
-     * @param gadget 		Name of the gadget to check if available
-     * @return 	‘false’ if the gadget is missing, and ‘true’ otherwise
+     *
+     * @param gadget Name of the gadget to check if available
+     * @return ‘false’ if the gadget is missing, and ‘true’ otherwise
      */
-	boolean getItem(String gadget){
-		//TODO: Implement this
-		return true;
-	}
+    public boolean getItem(String gadget) {
+        return gadgets.contains(gadget);
+    }
 
-	/**
-	 *
-	 * <p>
-	 * Prints to a file name @filename a serialized object List<Gadget> which is a
-	 * List of all the reports in the diary.
-	 * This method is called by the main method in order to generate the output.
-	 */
-	public void printToFile(String filename){
-		//TODO: Implement this
-	}
+    /**
+     * <p>
+     * Prints to a file name @filename a serialized object List<Gadget> which is a
+     * List of all the reports in the diary.
+     * This method is called by the main method in order to generate the output.
+     */
+    public void printToFile(String filename) throws IOException {
+        FileWriter fileWriter = new FileWriter(filename);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        for (String gadget : gadgets){
+        	printWriter.println(gadget);
+		}
+        printWriter.close();
+    }
 }
--- src/test/java/bgu/spl/mics/InventoryTest.java
+++ src/test/java/bgu/spl/mics/InventoryTest.java
@@ -1,4 +1,69 @@
 package test.java.bgu.spl.mics;
 
+import main.java.bgu.spl.mics.application.passiveObjects.Inventory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+
+
 public class InventoryTest {
+
+    private List<String> gadgets;
+    private Inventory inventory;
+
+    @BeforeEach
+    public void setUp() {
+        inventory = Inventory.getInstance();
+        gadgets = inventory.getGadgets();
+    }
+
+    @Test
+    void Load() {
+        try {
+            inventory.load(null);
+            fail("Can't load a null array");
+        }catch(Exception e){
+            //Test pass
+        }
+        String[] list = {"car", "gun", "hammer", "pistol"};
+        inventory.load(list);
+        assertTrue(gadgets.containsAll(Arrays.asList(list)), "All the gadgets should be in gadgets");
+        // Not needed but safer
+        gadgets.removeAll(Arrays.asList(list));
+        assertTrue(gadgets.isEmpty(), "gadgets should be empty");
+    }
+
+    @Test
+    void GetItem() {
+        String object = "gun";
+        assertFalse(inventory.getItem(object));
+        gadgets.add(object);
+        assertTrue(inventory.getItem(object));
+        gadgets.remove(object);
+        assertFalse(inventory.getItem(object));
+    }
+
+    @Test
+    void printToFile() throws IOException {
+        String[] list = {"car", "gun", "hammer", "pistol"};
+        gadgets.addAll(Arrays.asList(list));
+        inventory.printToFile("output.txt");
+
+        File file = new File("output.txt");    //creates a new file instance
+        FileReader fr = new FileReader(file);   //reads the file
+        BufferedReader br = new BufferedReader(fr);  //creates a buffering character input stream
+        String line;
+        while((line=br.readLine())!=null){
+            assertTrue(gadgets.contains(line));
+        }
+        fr.close();    //closes the stream and release the resources
+    }
 }
